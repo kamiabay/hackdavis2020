0deaeb04f9ef417fb63b5de34f893f37c5af4253
kamiabay <kamiab.ayani@gmail.com>
2020-01-18 14:48:05 -0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:danger/landing.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(Myapp());
+}
+class Myapp extends StatefulWidget {
+  @override
+  _MyappState createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: mainScreen(),
+        theme:  ThemeData(
+          buttonColor: Colors.red,
+        ),
+      );
+  }
+  mainScreen() {
+    return Landing();
+  }
+}
+
